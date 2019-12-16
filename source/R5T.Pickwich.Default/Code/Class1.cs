2c13f01acfdedfbac15d782ecc9db834131f6fa3
MinexAutomation <minex.automation@gmail.com>
2019-12-16 14:12:30 -0800

    Implemented the C# Visual Studio project file path provider.

--- source/R5T.Pickwich.Default/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Pickwich.Default
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Pickwich.Default/Code/Services/Implementations/CSharpVisualStudioProjectFilePathProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using R5T.Lombardy;
+using R5T.Pickwich.Types;
+
+
+namespace R5T.Pickwich.Default
+{
+    public class CSharpVisualStudioProjectFilePathProvider : IVisualStudioProjectFilePathProvider
+    {
+        private IFileNameOperator FileNameOperator { get; }
+        private IStringlyTypedPathOperator StringlyTypedPathOperator { get; }
+
+
+        public CSharpVisualStudioProjectFilePathProvider(IFileNameOperator fileNameOperator, IStringlyTypedPathOperator stringlyTypedPathOperator)
+        {
+            this.FileNameOperator = fileNameOperator;
+            this.StringlyTypedPathOperator = stringlyTypedPathOperator;
+        }
+
+        public string GetVisualStudioProjectFilePath(string projectDirectoryPath, string projectName)
+        {
+            var projectFileName = this.FileNameOperator.GetFileName(projectName, FileExtensions.CSharpProjectFileExtension);
+
+            var projectFilePath = this.StringlyTypedPathOperator.GetFilePath(projectDirectoryPath, projectFileName);
+            return projectFilePath;
+        }
+    }
+}
